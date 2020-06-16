63d7b676fefcde464b0a7e91015721f0971ce83d
Saular Raffi <fo6197@wayne.edu>
2020-06-15 22:28:46 -0400

    null

---
+++ Reverse Integer.py
@@ -0,0 +1,25 @@
+class Solution(object):
+
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        
+        neg = False
+
+        if x < 0:
+            neg = True
+            x = x * -1
+
+        if x == 0 or x > 2 ** 31 - 1:
+            return 0
+
+        rev = str(x)[::-1]
+
+        if int(rev) > 2 ** 31 - 1:
+            return 0
+        elif neg:
+            return int(rev) * -1
+        else:
+            return int(rev)
